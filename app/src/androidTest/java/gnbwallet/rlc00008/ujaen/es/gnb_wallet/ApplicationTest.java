c39193157da1d6f8643642695e6c7673646252a9
Ricardo Lechuga <ricardo.externos@mobilers.es>
2016-05-02 22:11:08 +0200

    initial import

---
+++ app/src/androidTest/java/gnbwallet/rlc00008/ujaen/es/gnb_wallet/ApplicationTest.java
@@ -0,0 +1,13 @@
+package gnbwallet.rlc00008.ujaen.es.gnb_wallet;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+	public ApplicationTest() {
+		super(Application.class);
+	}
+}
---
+++ app/src/main/java/gnbwallet/rlc00008/ujaen/es/gnb_wallet/MainActivity.java
@@ -0,0 +1,13 @@
+package gnbwallet.rlc00008.ujaen.es.gnb_wallet;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+}
